9353046eb37a8542d0bd9bea746f1db13a132c57
jurineide <jurineidesouza@gmail.com>
2024-01-11 11:10:32 -0300

    creating app files

---
+++ src/main/java/br/com/giovannapizzeria/giovannaPizzeria/GiovannaPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.giovannapizzeria.giovannaPizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiovannaPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiovannaPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/giovannapizzeria/giovannaPizzeria/GiovannaPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovannapizzeria.giovannaPizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiovannaPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
