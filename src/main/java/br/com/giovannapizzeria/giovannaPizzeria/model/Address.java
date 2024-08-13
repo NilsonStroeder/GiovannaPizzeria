b08600dde05ff8dabdf2bb1ea2192237a3d6e39b
Nilson <nilson_sf@yahoo.com.br>
2024-08-12 22:02:13 -0300

    implementando classe address

--- src/main/java/br/com/giovannapizzeria/giovannaPizzeria/model/Address.java
+++ src/main/java/br/com/giovannapizzeria/giovannaPizzeria/model/Address.java
@@ -1,4 +1,17 @@
 package br.com.giovannapizzeria.giovannaPizzeria.model;
 
+import lombok.Data;
+
+@Data
 public class Address {
+
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String Estado;
+    private String pais;
+    private String cep;
+
 }
